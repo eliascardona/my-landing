77ecf53042f3d0197355350fdb0be0eacc75df90
Elías <eliascardonarod@gmail.com>
2022-07-27 07:42:09 -0500

    Commit incial, subiendo nuestro repositorio a GitHub

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8000
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.listen(port)
+
+console.log(`Server started at http://localhost:${port}`)
